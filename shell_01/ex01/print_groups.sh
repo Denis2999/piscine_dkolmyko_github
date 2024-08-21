915d57b18aa4fad65b94a5f4e6817df8f966fa0b
Denis <denkolmykov@gmail.com>
2024-08-21 10:00:19 +0200

    Changed shell_00/ex07

--- shell_01/ex01/print_groups.sh
+++ shell_01/ex01/print_groups.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-FT_USER = nours
+#FT_USER = nours
 # FT_USER = daemon
 
-groups $FT_USER | tr ' ' ',' | tr -d '\n'
+#groups $FT_USER | tr ' ' ',' | tr -d '\n'
+groups $FT_USER | tr ' ' ','
+
