b4c2235ff6c97abc3fbc409b136b3a2ba5f67c71
Denis <denkolmykov@gmail.com>
2024-09-04 19:18:46 +0200

    c_04 commit, updated to Normiennete rules. Ready to defend

--- c_04/ex03/ft_atoi.c
+++ c_04/ex03/ft_atoi.c
@@ -6,44 +6,46 @@
 /*   By: dkolmyko <dkolmyko@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 13:24:10 by dkolmyko          #+#    #+#             */
-/*   Updated: 2024/09/04 17:56:27 by dkolmyko         ###   ########.fr       */
+/*   Updated: 2024/09/04 18:51:16 by dkolmyko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <stdio.h>
 
-int ft_atoi(char *str)
+int	ft_atoi(char *str)
 {
-	int i = 0, number_part = 1, sign = 1;
-	int int_result = 0;
+	int	i;
+	int	number_part;
+	int	sign;
+	int	int_result;
+	int	digit_value;
 
+	i = 0;
+	number_part = 1;
+	sign = 1;
+	int_result = 0;
 	while (str[i] != '\0' && number_part)
 	{
 		if (str[i] == '-')
-		{
 			sign = -sign;
-		}
 		if ('0' <= str[i] && str[i] <= '9')
 		{
-			int digit_value = str[i] - '0';
+			digit_value = str[i] - '0';
 			int_result = int_result * 10 + digit_value;
 			if ('0' > str[i + 1] || str[i + 1] > '9')
-			{
 				number_part = 0;
-			}
 		}
 		i++;
 	}
-
-	return int_result * sign;
+	return (int_result * sign);
 }
 
-int main()
-{
-	// char value[] = " ---+--+1234ab567";
-	char value[] = " -=-=-=-=-=--r908f9huji0hfui-+1234ab567";
+// int	main()
+// {
+// 	// char value[] = " ---+--+1234ab567";
+// 	char value[] = " -=-=-=-=-=--r908f9huji0hfui-+1234ab567";
 
-	printf("%d\n", ft_atoi(value));
-	return 0;
-}
+// 	printf("%d\n", ft_atoi(value));
+// 	return 0;
+// }
--- c_04/ex04/ft_putnbr_base.c
+++ c_04/ex04/ft_putnbr_base.c
@@ -6,16 +6,16 @@
 /*   By: dkolmyko <dkolmyko@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 17:12:10 by dkolmyko          #+#    #+#             */
-/*   Updated: 2024/09/04 17:50:26 by dkolmyko         ###   ########.fr       */
+/*   Updated: 2024/09/04 19:18:28 by dkolmyko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
 
-int ft_strlen(char *str)
+int	ft_strlen(char *str)
 {
-	int len;
+	int	len;
 
 	len = 0;
 	while (str[len] != '\0')
@@ -25,66 +25,75 @@ int ft_strlen(char *str)
 	return (len);
 }
 
-int is_valid_base(char *base)
+int	is_valid_base(char *base)
 {
-	int base_len = ft_strlen(base);
+	int	base_len;
+	int	i;
+	int	j;
 
+	base_len = ft_strlen(base);
 	if (base_len < 2)
-	{
-		return 0;
-	}
-
-	int i = 0;
+		return (0);
+	i = 0;
 	while (base[i] != '\0')
 	{
 		if (base[i] == '+' || base[i] == '-')
 		{
-			return 0;
+			return (0);
 		}
-		int j = i + 1;
+		j = i + 1;
 		while (base[j] != '\0')
 		{
 			if (base[i] == base[j])
-			{
-				return 0;
-			}
+				return (0);
 			j++;
 		}
 		i++;
 	}
-	return 1;
+	return (1);
 }
 
-void ft_putnbr_base(int nbr, char *base)
+void	convert_to_base(int nbr, char *base, char *result)
 {
-	int base_len = ft_strlen(base);
-	char result[32];
-	int i = 0;
+	int	base_len;
+	int	i;
+	int	modulo;
 
+	base_len = ft_strlen(base);
+	i = 0;
+	while (nbr != 0)
+	{
+		modulo = nbr % base_len;
+		result[i] = base[modulo];
+		nbr /= base_len;
+		i++;
+	}
+	if (i == 0)
+	{
+		result[i++] = base[0];
+	}
+	result[i] = '\0';
+}
+
+void	ft_putnbr_base(int nbr, char *base)
+{
+	int		base_len;
+	int		i;
+	int		modulo;
+	char	result[32];
+
+	i = 0;
+	base_len = ft_strlen(base);
 	if (!is_valid_base(base))
 	{
-		return;
+		return ;
 	}
-
 	if (nbr < 0)
 	{
 		write(1, "-", 1);
 		nbr = -nbr;
 	}
-
-	while (nbr != 0)
-	{
-		int modulo = nbr % base_len;
-		result[i] = base[modulo];
-		nbr /= base_len;
-		i++;
-	}
-
-	if (i == 0)
-	{
-		result[i++] = base[0];
-	}
-
+	convert_to_base(nbr, base, result);
 	while (--i >= 0)
 	{
 		write(1, &result[i], 1);
@@ -92,13 +101,13 @@ void ft_putnbr_base(int nbr, char *base)
 	write(1, "\n", 1);
 }
 
-int main()
-{
-	// int number = 7905690;
-	int number = 42;
+// int main()
+// {
+// 	// int number = 7905690;
+// 	int number = 42;
 
-	ft_putnbr_base(number, "0123456789");
-	ft_putnbr_base(number, "01");
-	ft_putnbr_base(number, "0123456789ABCDEF");
-	ft_putnbr_base(number, "poneyvif");
-}
+// 	ft_putnbr_base(number, "0123456789");
+// 	ft_putnbr_base(number, "01");
+// 	ft_putnbr_base(number, "0123456789ABCDEF");
+// 	ft_putnbr_base(number, "poneyvif");
+// }
